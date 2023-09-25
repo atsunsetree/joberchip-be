96d6d7efc2fc2daa28826958d215dc2df1882e84
atsunsetree <atsunsetree529@gmail.com>
2023-09-25 14:06:40 +0900

    feat: ImageBlock 레포지토리 추가

--- api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/repository/ImageBlockRepository.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/repository/ImageBlockRepository.java
@@ -1,7 +1,14 @@
 package kr.joberchip.server.v1.space.block.repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+
 import kr.joberchip.core.space.block.ImageBlock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ImageBlockRepository extends JpaRepository<ImageBlock, UUID> {}
+@Repository
+public interface ImageBlockRepository extends JpaRepository<ImageBlock, UUID> {
+    Optional<ImageBlock> findByObjectId(UUID id);
+}
