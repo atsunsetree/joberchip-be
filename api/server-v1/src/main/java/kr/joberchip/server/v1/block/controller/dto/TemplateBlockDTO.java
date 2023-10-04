5acce0f3ddf7b7e8b6a51b28ae1ebe0b1c344087
atsunsetree <atsunsetree529@gmail.com>
2023-10-04 10:14:44 +0900

    [api-server-v1] feat: TemplateBlockDTO 추가

--- api/server-v1/src/main/java/kr/joberchip/server/v1/block/controller/dto/TemplateBlockDTO.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/block/controller/dto/TemplateBlockDTO.java
@@ -1,6 +1,15 @@
 package kr.joberchip.server.v1.block.controller.dto;
 
-public record TemplateBlockDTO(
-        String title,
-        String description
-) {}
+import kr.joberchip.core.share.OnScreenLocation;
+import kr.joberchip.core.share.block.TemplateBlock;
+
+public record TemplateBlockDTO(String title,
+                               String description,
+                               Integer x,
+                               Integer y,
+                               Integer width,
+                               Integer height) {
+    public TemplateBlock toEntity() {
+        return TemplateBlock.of(title, description);
+    }
+}
