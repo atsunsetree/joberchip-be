a7aaaffb6f82cc7f77d523ee9895ad822ea942ab
atsunsetree <atsunsetree529@gmail.com>
2023-10-04 14:10:37 +0900

    [api-server-v1] feat: 템플릿 블록 entity 리퀘스트 형식에 맞게 수정

--- api/server-v1/src/main/java/kr/joberchip/server/v1/block/controller/dto/TemplateBlockDTO.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/block/controller/dto/TemplateBlockDTO.java
@@ -1,14 +1,15 @@
 package kr.joberchip.server.v1.block.controller.dto;
 
 import kr.joberchip.core.share.block.TemplateBlock;
+import org.springframework.web.bind.annotation.RequestParam;
 
-public record TemplateBlockDTO(String title,
-                               String description,
-                               Integer x,
-                               Integer y,
-                               Integer width,
-                               Integer height) {
+public record TemplateBlockDTO(@RequestParam String title,
+                               @RequestParam String description,
+                               @RequestParam Integer x,
+                               @RequestParam Integer y,
+                               @RequestParam Integer w,
+                               @RequestParam Integer h) {
     public TemplateBlock toEntity() {
-        return TemplateBlock.of(title, description);
+        return TemplateBlock.of(title, description, x, y, w, h);
     }
 }
---
+++ libs/core/src/main/java/kr/joberchip/core/share/block/TemplateBlock.java
@@ -0,0 +1,51 @@
+package kr.joberchip.core.share.block;
+
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import kr.joberchip.core.share.BaseObject;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "template_block_tb")
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class TemplateBlock extends BaseObject {
+  @Column(name = "title")
+  private String title;
+
+  @Column(name = "description")
+  private String description;
+
+  public static TemplateBlock of(String title, String description) {
+    return new TemplateBlock(title, description);
+  }
+
+  public static TemplateBlock of(String title, String description, Integer x, Integer y, Integer w, Integer h) {
+    TemplateBlock generated = new TemplateBlock(title, description);
+
+    generated.setX(x);
+    generated.setY(y);
+    generated.setWidth(w);
+    generated.setHeight(h);
+
+    return generated;
+  }
+
+  public void modifyTitle(String title) {
+    this.title = title;
+  }
+
+  public void modifyDescription(String description) {
+    this.description = description;
+  }
+
+  public UUID getTemplateBlockId() {
+    return this.objectId;
+  }
+}
